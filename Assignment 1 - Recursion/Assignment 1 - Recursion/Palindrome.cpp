a8c30da38e417fee53a20ad9ae659e8500128fe0
smoreno11 <60587113+smoreno11@users.noreply.github.com>
2023-01-25 11:51:19 -0800

    Code
    ds

--- Assignment 1 - Recursion/Assignment 1 - Recursion/Palindrome.cpp
+++ Assignment 1 - Recursion/Assignment 1 - Recursion/Palindrome.cpp
@@ -1,3 +1,10 @@
+/******************************************************************************
+* AUTHOR : Saul Moreno
+* ASSIGNMENT#1 : Recursion
+* CLASS : CS1D
+* SECTION : MW 2:00pm
+* DUE DATE : 1/29/23
+******************************************************************************/
 #include "Palindrome.h"
 
 Palindrome::Palindrome() {
